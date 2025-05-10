0387c647cf9e364a37df208d28d28a86477968f6
Marci Prescott <mjprescott@me.com>
2025-05-10 17:22:05 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div>
+      <div>Personal Digital Assistant</div>
+      <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Siri" handle="@siri01" />
+    </div>
+  );
+}
+export default App;
