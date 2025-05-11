9622f370cb56a0f9e771ab67413d576936f83c1f
Marci Prescott <mjprescott@me.com>
2025-05-11 17:29:29 -0600

    Finished pda project

--- ProfileCard.js
+++ ProfileCard.js
@@ -1,9 +1,22 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle, image, description }) {
+  // This is a functional component that takes in props
   return (
-    <div>
-      <div>Title is {props.title}</div>
-      <div>Handle is {props.handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4"> {title}</p>
+          <p className="subtitle is-6"> {handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
+// ProfileCard.js
 export default ProfileCard;
